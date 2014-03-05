b21d0604d492d948cbc9054f820be56172429730
Matthias Urlichs <matthias@urlichs.de>
2014-03-05 06:49:21 +0100

    requests HTTP works, HTTPS does not yet

--- wsgi_intercept/requests_intercept.py
+++ wsgi_intercept/requests_intercept.py
@@ -2,30 +2,25 @@
 intercept HTTP connections that use requests
 """
 
-from . import WSGI_HTTPConnection, wsgi_fake_socket
-from requests.packages.urllib3.connectionpool import (HTTPConnectionPool,
-        HTTPSConnectionPool)
-from requests.packages.urllib3.connection import (HTTPConnection,
-        HTTPSConnection)
+from . import WSGI_HTTPConnection, WSGI_HTTPSConnection, wsgi_fake_socket
+try:
+    from requests.packages.urllib3 import connectionpool
+except ImportError:
+    from urllib3 import connectionpool
 
+HTTPConnection = connectionpool.HTTPConnection
+HTTPSConnection = connectionpool.HTTPSConnection
 
 InterceptorMixin = WSGI_HTTPConnection
+InterceptorMixinS = WSGI_HTTPSConnection
 wsgi_fake_socket.settimeout = lambda self, timeout: None
 
 
-class HTTP_WSGIInterceptor(InterceptorMixin, HTTPConnection):
-    pass
-
-
-class HTTPS_WSGIInterceptor(InterceptorMixin, HTTPSConnection):
-    pass
-
-
 def install():
-    HTTPConnectionPool.ConnectionCls = HTTP_WSGIInterceptor
-    HTTPSConnectionPool.ConnectionCls = HTTPS_WSGIInterceptor
+    connectionpool.HTTPConnection = InterceptorMixin
+    connectionpool.HTTPSConnection = InterceptorMixinS
 
 
 def uninstall():
-    HTTPConnectionPool.ConnectionCls = HTTPConnection
-    HTTPSConnectionPool.ConnectionCls = HTTPSConnection
+    connectionpool.HTTPConnection = HTTPConnection
+    connectionpool.HTTPSConnection = HTTPSConnection
